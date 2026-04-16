eadf80bb77379c920f323f9708e5c3737e267b15
Kevin Fu <114120665+kevinfu21@users.noreply.github.com>
2026-04-15 23:47:21 -0400

    q2 fixed hopefully

--- question2/q2.c
+++ question2/q2.c
@@ -172,7 +172,7 @@ void ungetch(int c) // push character back on input
 void treeprint(struct tnode *p) {
     if (p != NULL) {
         treeprint(p->left);
-        printf("%4d %s\n", p->count, p->word);
+        printf("%d %s\n", p->count, p->word);
         treeprint(p->right);
     }
 }
